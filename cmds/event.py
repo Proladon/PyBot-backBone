072a615ebc2fdec2175b7bb75b7e3c6d35d43a73
Proladon <proladon@gmail.com>
2019-11-28 14:38:05 +0800

    update_jdata comment

--- cmds/event.py
+++ cmds/event.py
@@ -13,12 +13,10 @@ class Event(Cog_Extension):
 		'''指令錯誤觸發事件'''
 		error_command = '{0}_error'.format(ctx.command)
 		try:
-			# 檢查是否有 Custom Error Handler
-			if hasattr(Errors, error_command):
+			if hasattr(Errors, error_command): # 檢查是否有 Custom Error Handler
 				await Errors.error_command()
 				return
-			# 使用 Default Error Handler
-			else:
+			else: # 使用 Default Error Handler
 				await Errors.default_error(self, ctx, error)
 		except AttributeError:
 			pass
--- core/classes.py
+++ core/classes.py
@@ -8,7 +8,11 @@ class Cog_Extension(commands.Cog):
 
 class Gloable_Func():
 	def update_jdata(self, key, data, type='default', mode='update'):
-		'''更新 Jdata 功能'''
+		'''
+		更新 Jdata 功能\n
+		type: default / list\n
+		mode: update / delete
+		'''
 		with open('setting.json', 'r', encoding='utf8') as jfile:
 			jdata = json.load(jfile)
 			if mode == 'update':
