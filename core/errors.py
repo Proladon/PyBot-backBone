f4f11c0e5a4e691029d66f0a8bff4da903dc7acc
Proladon <35193493+Proladon@users.noreply.github.com>
2020-05-11 14:31:46 +0800

    Update errors.py

--- core/errors.py
+++ core/errors.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from core.classes import Cog_Extension, Logger
+from cmds.main import Main #導入 Main Cog
 import json, asyncio
 
 with open('setting.json', 'r', encoding='utf8') as jfile:
@@ -8,6 +9,15 @@ with open('setting.json', 'r', encoding='utf8') as jfile:
 
 class Errors():
 	# 自訂 Error Handler
+	
+	@Main.sayd.error
+	'''Main.sayd 指令錯誤處理'''
+	async def sayd_error(self, ctx, error):
+		if isinstance(error, commands.MissingRequiredArgument):
+			await ctx.send('請輸入要覆誦的訊息')
+			Logger.log(self, ctx, error)
+			
+		
 
 
 	# 預設 Error Handler
